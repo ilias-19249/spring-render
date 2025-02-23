8958e3997ec3d71ba1c12fae86298910102371ce
unknown <iliasna2004@gmail.com>
2025-02-23 11:35:44 +0000

    second commit

---
+++ src/test/java/com/example/springrender/SpringrenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
